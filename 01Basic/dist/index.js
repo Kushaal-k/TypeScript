ff1d592195a693741f96eb9ec5bffffa35b63082
Kushaal Kankane <kushaalkankane12345@gmail.com>
2025-12-14 18:48:23 +0000

    Started Learning TypeScript

--- 01Basic/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(person) {
-    return `Hello, ${person}!`;
-}
-const usernmae = "Kushaal";
-console.log(greet(usernmae));
-//# sourceMappingURL=index.js.map
