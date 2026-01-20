4cec2300016f148b5d2f8f0c74decc1429072393
Kushaal Kankane <kushaalkankane12345@gmail.com>
2026-01-20 14:43:59 +0000

    OOPs in TS

--- 01Basic/src/OOP.ts
+++ 01Basic/src/OOP.ts
@@ -44,4 +44,60 @@ const c = new Chai();
 
 class Wallet {
     #balance = 100 //Private using #
+
+    getBalance(){
+        return this.#balance
+    }
 }
+
+const w = new Wallet();
+
+class Cup{
+    readonly capacity: number = 250
+}
+
+class ModernChai {
+    private _sugar = 2 //
+
+    get sugar(){
+        return this._sugar
+    }
+
+    set sugar(value: number){
+        this._sugar = value
+    }
+}
+
+
+class EkChai {
+    static shopName = "Chai cafe"
+
+    constructor(public flavour: string){}
+}
+
+console.log(EkChai.shopName);
+
+abstract class Drink {
+    abstract make(): void
+}
+
+class MyChai extends Drink {
+    make(): void {
+        console.log("I am an abstract function");
+    }
+}
+
+class Heater{
+    heat(){}
+}
+
+class ChaiMaker{
+    constructor(private heater: Heater){
+        
+    }
+
+    make(){
+        this.heater.heat
+    }
+}
+
